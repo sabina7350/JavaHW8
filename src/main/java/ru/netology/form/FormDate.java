58db4bc427bf99ed3046b2574fd9ab93f2ad564b
sabibi_k <sabina7350@gmail.com>
2024-02-17 15:55:52 +0300

    AddForm

---
+++ src/main/java/ru/netology/form/FormDate.java
@@ -0,0 +1,8 @@
+package ru.netology.form;
+
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+
+}
---
+++ src/main/java/ru/netology/form/Post.java
@@ -0,0 +1,12 @@
+package ru.netology.form;
+
+public class Post {
+    public FormDate birthday;
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+
+}
